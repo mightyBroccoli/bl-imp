e434e449aa6f6d6e0b7bd68d493c4fec7b6f2b38
nico wellpott <nico@magicbroccoli.de>
2023-01-13 21:51:54 +0100

    build(setup): move install_requires into cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup
-
-setup(
-    name="bl-imp",
-    install_requires=[
-        "appdirs>=1.4",
-        "requests>=2.25",
-        "ruamel.yaml>=0.17",
-        "ruamel.yaml.clib>=0.2",
-        "urllib3>=1.26",
-    ],
-)
+setup()
